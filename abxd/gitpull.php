81176cdff7054897b436d2d237dfd9f25a715bae
Mega-Mario <thetotalworm@gmail.com>
2012-02-12 00:08:58 +0100

    Pure fancy: prettier gitpull.php output

--- abxd/gitpull.php
+++ abxd/gitpull.php
@@ -4,6 +4,14 @@ include("lib/common.php");
 if($loguser['powerlevel'] < 3)
 	Kill(__("You're not admin. There is nothing for you here."));
 	
-system("git pull");
+$output = array();
+exec("git pull", $output);
+echo '<div style="width: 50%; margin-left: auto; margin-right: auto; background: black; border: 1px solid #0f0; color: #0f0; font-family: \'Consolas\', \'Lucida Console\', \'Courier New\', monospace;">';
+
+if (empty($output)) echo '<em>(no output)</em>';
+else
+	foreach ($output as $line) echo htmlspecialchars($line).'<br>';
+
+echo '</div>';
 
 ?>
