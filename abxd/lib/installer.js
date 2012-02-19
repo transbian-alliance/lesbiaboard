55ac6abf82210a2a2658277ce1ce7573034f07b4
Nina <nina@blackbox.(none)>
2012-02-19 14:05:18 +0100

    Fixed the SQL connectivity check.

--- abxd/lib/installer.js
+++ abxd/lib/installer.js
@@ -34,14 +34,14 @@ window.onload = function() {
 		if (page == numPages) $("#nextPageButton").attr("disabled");
 		$("#prevPageButton").removeAttr("disabled");
 	});
-	$("#installButton").click(function() { $(this).fadeOut(1000, function() {doInstall();}); });
+	$("#installButton").click(function() { doInstall(); });
 }
 
 function checkSqlConnection(attemptCreate) {
 	url = "install/checksql.php";
 	if (attemptCreate == true) url += "?attemptCreate=true";
 	$.post(url, {
-		sqlServer: $('#sqlServer').val(),
+		sqlServerAddress: $('#sqlServerAddress').val(),
 		sqlUserName: $('#sqlUserName').val(),
 		sqlPassword: $('#sqlPassword').val(),
 		sqlDbName: $('#sqlDbName').val()
@@ -51,8 +51,29 @@ function checkSqlConnection(attemptCreate) {
 	});
 }
 
+success = "";
 function doInstall() {
-	$("#prevPageButton, #nextPageButton").hide(250);
+	$("#prevPageButton, #nextPageButton").slideUp(250);
 	$("#progress").slideUp(250);
 	$("#page4").html('<div class="center" style="padding-top: 100px; font-style: italic;"><div class="pollbarContainer" style="width: 50%; margin: 12pt auto;">Installing. Please wait.</div></div>');
+	$.post("install/doinstall.php", {
+		action: "Install",
+		dbserv: $('#sqlServerAddress').val(),
+		dbuser: $('#sqlUserName').val(),
+		dbpass: $('#sqlPassword').val(),
+		dbname: $('#sqlDbName').val(),
+		boardname: $("#boardName").val(),
+		logoalt: $("boardLogoAlt").val(),
+		logotitle: $("boardLogoTitle").val(),
+		defaultgroups: $("setUpDefaultUserGroups").val(),
+		addbase: $("createDefaultForums").val(),
+		htmltidy: $("useHTMLTidy").val()
+	}, function(data) {
+		$("#page4").html(data);
+	});
+}
+
+function reenableControls() {
+	$('#prevPageButton, #nextPageButton').show(250);
+	$('#progress').show(250);
 }
