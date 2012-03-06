4b4f0f780c67b474c2a6448aded8dbfd283a9b6f
Dirbaio <dirbaio@dirbaio.net>
2012-03-06 18:55:12 +0100

    CSS cleanup (part 1)

--- abxd/layout.php
+++ abxd/layout.php
@@ -10,7 +10,7 @@
 	<meta name="keywords" content="<?php print $metaKeywords; ?>" />
 	<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
 	<link rel="stylesheet" type="text/css" href="<?php print resourceLink("css/common.css");?>" />
-	<link rel="stylesheet" type="text/css" id="theme_css" href="<?php print $layout_themefile; ?>" />
+	<link rel="stylesheet" type="text/css" id="theme_css" href="<?php print $layout_themefile; ?>" /> 
 
 	<script type="text/javascript" src="<?php print resourceLink("lib/tricks.js");?>"></script>
 	<script type="text/javascript" src="<?php print resourceLink("lib/jquery.js");?>"></script>
