effa57ce6f93829c16fd6429e9e2765ec39bed5c
Dirbaio <dirbaio@dirbaio.net>
2012-03-21 18:48:19 +0100

    Yay, categorized uploader is now finished.

---
+++ abxd/plugins/categoryuploader/page_uploader.php
@@ -0,0 +1,355 @@
+<?php
+
+$title = __("Uploader");
+
+AssertForbidden("viewUploader");
+
+$rootdir = "uploader";
+//if(!is_file($rootdir."/.htaccess"))
+{
+	$here = $_SERVER['SCRIPT_FILENAME'];
+	$here = substr($here, 0, strrpos($here, '/') + 1);
+	$here = str_replace($_SERVER['DOCUMENT_ROOT'], '', $here);
+	file_put_contents($rootdir."/.htaccess", "RewriteEngine On\nRewriteRule ^(.+)$ ".$here."get.php?file=$1 [PT,L,QSA]\nRewriteRule ^$ ".$here."get.php?error [PT,L,QSA]");
+}
+
+if($uploaderWhitelist)
+	$goodfiles = explode(" ", $uploaderWhitelist);
+
+$badfiles = array("html", "htm", "php", "php2", "php3", "php4", "php5", "php6", "htaccess", "htpasswd", "mht", "js", "asp", "aspx", "cgi", "py", "exe", "com", "bat", "pif", "cmd", "lnk", "wsh", "vbs", "vbe", "jse", "wsf", "msc", "pl", "rb", "shtm", "shtml", "stm", "htc");
+
+if(isset($_POST['action']))
+	$_GET['action'] = $_POST['action'];
+if(isset($_POST['fid']))
+	$_GET['fid'] = $_POST['fid'];
+
+$quota = $uploaderCap * 1024 * 1024;
+$pQuota = $personalCap * 1024 * 1024;
+$totalsize = foldersize($rootdir);
+
+$maxSizeMult = $uploaderMaxFileSize * 1024 * 1024;
+
+if($_GET['action'] == "uploadform")
+{
+
+	$cat = getCategory($_GET["cat"]);
+	if (!is_numeric($_GET["cat"]))
+		Kill('Invalid category');
+
+	$cat = getCategory($_GET["cat"]);
+
+	MakeCrumbs(array("Main"=>"./", 
+					"Uploader"=>actionLink("uploader"), 
+					$cat["name"] => actionLink("uploaderlist", "", "cat=".$cat["id"]), 
+					"Upload new file" => actionLink("uploader", "", "action=uploadforum&cat=".$cat["id"])), $links);
+
+	if($loguserid && IsAllowed("useUploader"))
+	{
+		print format(
+		"
+		<script type=\"text/javascript\">
+			window.addEventListener(\"load\", function() { hookUploadCheck(\"newfile\", 1, {1}) }, false);
+		</script>
+		<form action=\"".actionLink("uploader")."\" method=\"post\" enctype=\"multipart/form-data\">
+			<input type='hidden' name='cat' value='${_GET["cat"]}'>
+			<table class=\"outline margin\">
+				<tr class=\"header0\">
+					<th colspan=\"4\">".__("Upload")."</th>
+				</tr>
+				<tr class=\"cell0\">
+					<td>File</td><td>
+						<input type=\"file\" id=\"newfile\" name=\"newfile\" style=\"width: 80%;\" />
+					</td>
+				</tr>
+				<tr class=\"cell1\">
+					<td>Description</td><td>
+						<input type=\"text\" name=\"description\" style=\"width: 80%;\" />
+					</td>
+				</tr>
+				<tr class=\"cell0\">
+					<td></td><td>
+						<input type=\"submit\" id=\"submit\" name=\"action\" value=\"".__("Upload")."\" disabled=\"disabled\" />
+					</td>
+				</tr>
+				<tr class=\"cell1 smallFonts\">
+					<td colspan=\"3\">
+						".__("The maximum upload size is {0} per file. You can upload the following types: {2}.")."
+						<div id=\"sizeWarning\" style=\"display: none; font-weight: bold\">".__("File is too large.")."</div>
+						<div id=\"typeWarning\" style=\"display: none; font-weight: bold\">".__("File is not an allowed type.")."</div>
+					</td>
+				</tr>
+			</table>
+		</form>
+		", BytesToSize($maxSizeMult), $maxSizeMult, $uploaderWhitelist);
+	
+	}
+}
+
+else if($_GET['action'] == __("Upload"))
+{
+	AssertForbidden("useUploader");
+	if($loguserid)
+	{
+		$cat = getCategory($_POST["cat"]);
+		$targetdir = $rootdir;
+		$quot = $quota;
+		$privateFlag = 0;
+		if($_POST['cat'] == -1)
+		{
+			$quot = $pQuota;
+			$targetdir = $rootdir."/".$loguserid;
+			$privateFlag = 1;
+		}
+		$totalsize = foldersize($targetdir);
+		
+		mkdir($targetdir);
+		$files = scandir($targetdir);
+		if(in_array($_FILES['newfile']['name'], $files))
+			Alert(format(__("The file \"{0}\" already exists. Please delete the old copy before uploading a new one."), $_FILES['newfile']['name']));
+		else
+		{
+			if($_FILES['newfile']['size'] == 0)
+			{
+				if($_FILES['newfile']['tmp_name'] == "")
+					Alert(__("No file given."));
+				else
+					Alert(__("File is empty."));
+			}
+			else if($_FILES['newfile']['size'] > $uploaderMaxFileSize * 1024 * 1024)
+			{
+				Alert(format(__("File is too large. Maximum size is {0}."), BytesToSize($uploaderMaxFileSize * 1024 * 1024)));
+			}
+			else
+			{
+				$fname = $_FILES['newfile']['name'];
+				$temp = $_FILES['newfile']['tmp_name'];
+				$size = $_FILES['size']['size'];
+				$parts = explode(".", $fname);
+				$extension = end($parts);
+				if($totalsize + $size > $quot)
+					Alert(format(__("Uploading \"{0}\" would break the quota."), $fname));
+				else if(in_array(strtolower($extension), $badfiles) || is_array($goodfiles) && !in_array(strtolower($extension), $goodfiles))
+				{
+					Alert(__("Forbidden file type."));
+				}
+				else
+				{
+					$description = strip_tags($_POST['description']);
+
+					$newID = FetchResult("SELECT id+1 FROM uploader WHERE (SELECT COUNT(*) FROM uploader u2 WHERE u2.id=uploader.id+1)=0 ORDER BY id ASC LIMIT 1");
+					if($newID < 1) $newID = 1;
+
+					Query("insert into uploader (id, filename, description, date, user, private, category) values (".$newID.", '".justEscape($fname)."', '".justEscape($description)."', ".time().", ".$loguserid.",".$privateFlag.",".$_POST["cat"].")");
+					copy($temp, $targetdir."/".$fname);
+					Report("[b]".$loguser['name']."[/] uploaded file \"[b]".$fname."[/]\"".($privateFlag ? " (privately)" : ""), $privateFlag); 
+
+					die(header("Location: ".actionLink("uploaderlist", "", "cat=".$_POST["cat"])));
+				}
+			}
+		}
+	}
+	else
+		Alert(__("You must be logged in to upload."));
+}
+else if($loguserid && $_GET['action'] == "multidel" && $_POST['del']) //several files
+{
+	$deleted = 0;
+	foreach($_POST['del'] as $fid => $on)
+	{
+		if($loguser['powerlevel'] > 2)
+			$check = FetchResult("select count(*) from uploader where id = ".$fid, 0, 0);
+		else
+			$check = FetchResult("select count(*) from uploader where user = ".$loguserid." and id = ".$fid, 0, 0);
+
+		if($check)
+		{
+			$entry = Fetch(Query("select * from uploader where id = ".$fid));
+			if($entry['private'])
+				@unlink($rootdir."/".$entry['user']."/".$entry['filename']);
+			else
+				@unlink($rootdir."/".$entry['filename']);
+			Query("delete from uploader where id = ".$fid);
+			$deleted++;
+		}
+	}
+	die(header("Location: ".actionLink("uploaderlist", "", "cat=".$_GET["cat"])));
+}
+else if($loguserid && $_GET['action'] == "multimove" && $_POST['del']) //several files
+{
+	
+	$moved = 0;
+	$newcat = $_POST['destcat'];
+	if (!is_numeric($newcat))
+		Kill('Invalid category ID');
+
+	foreach($_POST['del'] as $fid => $on)
+	{
+		if($loguser['powerlevel'] > 2)
+			$check = FetchResult("select count(*) from uploader where id = ".$fid, 0, 0);
+		else
+			$check = FetchResult("select count(*) from uploader where user = ".$loguserid." and id = ".$fid, 0, 0);
+
+		if($check)
+		{
+			if(!$entry['private'])
+			{
+				$entry = Fetch(Query("update uploader set `category` = $newcat where id = ".$fid));
+				$moved++;
+			}
+		}
+	}
+	die(header("Location: ".actionLink("uploaderlist", "", "cat=".$_GET["cat"])));
+}
+
+else if($_GET['action'] == "delete") //single file
+{
+	$fid = (int)$_GET['fid'];
+
+	if($loguser['powerlevel'] > 2)
+		$check = FetchResult("select count(*) from uploader where id = ".$fid, 0, 0);
+	else
+		$check = FetchResult("select count(*) from uploader where user = ".$loguserid." and id = ".$fid, 0, 0);
+	
+	if($check)
+	{
+		$entry = Fetch(Query("select * from uploader where id = ".$fid));
+		if($entry['private'])
+			@unlink($rootdir."/".$entry['user']."/".$entry['filename']);
+		else
+			@unlink($rootdir."/".$entry['filename']);
+		Query("delete from uploader where id = ".$fid);
+		Report("[b]".$loguser['name']."[/] deleted \"[b]".$entry['filename']."[/]\".", 1);
+		die(header("Location: ".actionLink("uploaderlist", "", "cat=".$_GET["cat"])));
+	}
+	else
+		Alert(__("No such file or not yours to mess with."));
+}
+else
+{
+	MakeCrumbs(array("Main"=>"./", 
+					"Uploader"=>actionLink("uploader")), $links);
+
+	$errormsg = __("No categories found.");
+	$entries = Query("select * from uploader_categories order by ord");
+
+	if(NumRows($entries) == 0)
+	{
+		print "
+		<table class=\"outline margin\">
+			<tr class=\"header0\">
+
+
+
+				<th colspan=\"7\">".__("Files")."</th>
+			</tr>
+			<tr class=\"cell1\">
+				<td colspan=\"4\">
+					".$errormsg."
+				</td>
+			</tr>
+		</table>
+		";
+	}
+	else
+	{
+		print 
+		"
+		<table class=\"outline margin width50\">
+			<tr class=\"header0\">
+				<th colspan=\"7\">".__("Categories")."</th>
+			</tr>
+		";
+
+		$cellClass = 0;
+				
+		while($entry = Fetch($entries))
+		{
+			$filecount = FetchResult("select count(*) from uploader where category = ".$entry['id'], 0, 0);
+		
+			print "<tr class=\"cell$cellClass\"><td>";
+			print actionLinkTag($entry['name'], "uploaderlist", "", "cat=".$entry['id']);
+			print "<br>";
+			print $entry['description'];
+			print "<br>";
+			print $filecount." files.";
+			print "<br>";
+			print "</td></tr>";
+			$cellClass = ($cellClass+1) % 2;
+		}
+		
+		if($loguserid)
+		{
+			$filecount = FetchResult("select count(*) from uploader where uploader.user = ".$loguserid." and uploader.private = 1", 0, 0);
+
+			print format("
+			<tr class=\"cell{0}\">
+				<td style='text-align:center; height:4px;'>
+				</td>
+			</tr>
+			<tr class=\"cell{0}\">
+				<td>
+					<a href=\"uploaderlist.php?cat={1}\">{2}</a><br /> {3}<br /> {4} files.
+				</td>
+			</tr>
+			",	$cellClass, -1, "Private files", "Only for you.", $filecount);
+		
+			$cellClass = ($cellClass+1) % 2;
+
+			if($loguser['powerlevel'] > 2)
+			{
+				$filecount = FetchResult("select count(*) from uploader where uploader.private = 1", 0, 0);
+				print format("
+				<tr class=\"cell{0}\">
+					<td style='text-align:center; height:4px;'>
+					</td>
+				</tr>
+				<tr class=\"cell{0}\">
+					<td>
+						<a href=\"uploaderlist.php?cat={1}\">{2}</a><br /> {3}<br /> {4} files.
+					</td>
+				</tr>
+				",	$cellClass, -2, "All private files", "", $filecount);
+			}
+		}
+		print "</table>";
+	}
+}
+
+//From the PHP Manual User Comments
+function foldersize($path)
+{
+	$total_size = 0;
+	$files = scandir($path);
+	$files = array_slice($files, 2);
+	foreach($files as $t)
+	{
+		$size = filesize($path . "/" . $t);
+		$total_size += $size;
+	}
+	return $total_size;
+}
+
+function getCategory($cat)
+{
+
+	if (!is_numeric($cat))
+		Kill('Invalid category');
+
+	if($cat >= 0)
+	{
+		$qCategory = "select * from uploader_categories where id=".$cat;
+		$rCategory = Query($qCategory);
+		if(NumRows($rCategory) == 0) Kill("Invalid category");
+		$rcat = Fetch($rCategory);
+	}
+	else if($cat == -1)
+		$rcat = array("id" => -1, "name" => "Private files");
+	else if($cat == -2)
+		$rcat = array("id" => -2, "name" => "All private files");
+	else
+		Kill('Invalid category');
+
+	return $rcat;
+}
+
+?>
---
+++ abxd/plugins/categoryuploader/page_uploaderlist.php
@@ -0,0 +1,209 @@
+<?php
+
+$title = __("Uploader");
+
+AssertForbidden("viewUploader");
+
+$rootdir = "uploader";
+
+if($uploaderWhitelist)
+	$goodfiles = explode(" ", $uploaderWhitelist);
+
+$badfiles = array("html", "htm", "php", "php2", "php3", "php4", "php5", "php6", "htaccess", "htpasswd", "mht", "js", "asp", "aspx", "cgi", "py", "exe", "com", "bat", "pif", "cmd", "lnk", "wsh", "vbs", "vbe", "jse", "wsf", "msc", "pl", "rb", "shtm", "shtml", "stm", "htc");
+
+function listCategory($cat)
+{
+	global $loguser, $loguserid, $rootdir, $userSelectUsers;
+	
+	if(isset($_GET['sort']) && $_GET['sort'] == "filename" || $_GET['sort'] == "date")
+		$skey = $_GET['sort'];
+	else
+		$skey = "date";
+
+	$sortOptions = "<div class=\"margin smallFonts\">".__("Sort order").": <ul class=\"pipemenu\">";
+	$sortOptions .= ($skey == "filename") 
+			?"<li>".__("Name")."</li>"
+			:actionLinkTagItem(__("Name"), "uploaderlist", "", "cat=${_GET["cat"]}&sort=filename");
+	$sortOptions .= ($skey == "date") 
+			?"<li>".__("Date")."</li>"
+			:actionLinkTagItem(__("Date"), "uploaderlist", "", "cat=${_GET["cat"]}&sort=date");
+	$sortOptions .= "</ul></div>";
+	$sdir = ($skey == "date") ? " desc" : " asc";
+
+
+	print $sortOptions;
+
+	if($cat == -1)
+		$condition = "uploader.user = ".$loguserid." and uploader.private = 1";
+	else if($cat == -2 && $loguser['powerlevel'] > 2)
+		$condition = "uploader.private = 1";
+	else
+		$condition = "uploader.private = 0 and uploader.category = $cat";
+	
+	$errormsg = __("The category is empty.");
+	if($cat < 0)
+		$errormsg = __("You have no private files.");
+		
+	$entries = Query("select uploader.*, $userSelectUsers from uploader left join users on uploader.user = users.id where $condition order by ".$skey.$sdir);
+
+	$checkbox = "";
+	if($loguserid)
+	{
+		$checkbox = "<input type=\"checkbox\" id=\"ca\" onchange=\"checkAll();\" />";
+		$checkbox = "<th style=\"width: 22px;\">$checkbox</th>";
+	}
+
+	if(NumRows($entries) == 0)
+	{
+		print "
+		<table class=\"outline margin\">
+			<tr class=\"header0\">
+				<th colspan=\"7\">".__("Files")."</th>
+			</tr>
+			<tr class=\"cell1\">
+				<td colspan=\"4\">
+					".$errormsg."
+				</td>
+			</tr>
+		</table>
+		";
+	}
+	else
+	{
+		print 
+		"
+		<table class=\"outline margin\">
+			<tr class=\"header0\">
+				<th colspan=\"7\">".__("Files")."</th>
+			</tr>
+			
+		";
+
+		print 	"
+			<tr class=\"header1\">
+				$checkbox
+				<th>
+					".__("File")."
+				</th>
+				<th>
+					".__("Description")."
+				</th>
+				<th>
+					".__("Size")."
+				</th>
+				<th>
+					".__("Uploader")."
+				</th>
+			</tr>
+		";
+
+		while($entry = Fetch($entries))
+		{
+			$delete = "";
+			$multidel = "";
+			if($loguserid)
+				$multidel = "<td><input type=\"checkbox\" name=\"delete[".$entry['id']."]\" disabled=\"disabled\" /></td>";
+			if($loguserid == $entry['user'] || $loguser['powerlevel'] > 2)
+			{
+				$delete = "&nbsp;<sup>"
+					.actionLinkTag("&#x2718;", "uploader", "", "action=delete&amp;fid=".$entry['id']."&amp;cat=".$_GET["cat"])
+					."</sup>";
+				$multidel = "<td><input type=\"checkbox\" name=\"del[".$entry['id']."]\" /></td>";
+			}
+			$cellClass = ($cellClass+1) % 2;
+			
+			$filepath = $rootdir."/".$entry['filename'];
+			if($entry['private'])
+				$filepath = $rootdir."/".$entry['user']."/".$entry['filename'];
+				
+			print format(			
+			"
+			<tr class=\"cell{0}\">
+				{7}
+				<td>
+					<a href=\"get.php?id={1}\">{2}</a>{3}
+				</td>
+				<td>
+					{4}
+				</td>
+				<td>
+					{5}
+				</td>
+				<td>
+					{6}
+				</td>
+			</tr>
+			",	$cellClass, $entry['id'], $entry['filename'], $delete, $entry['description'],
+				BytesToSize(@filesize($filepath)), UserLink($entry, "user"), $multidel);
+		}
+		
+		
+		if($loguserid)
+		{
+			$entries = Query("select * from uploader_categories order by ord");
+			$movelist = "";
+			
+			while($entry = Fetch($entries))
+			{
+				$movelist .= "<option value='${entry["id"]}'>${entry["name"]}</option>";
+			}
+			$movelist = "<select name='destcat' size='1'>$movelist</select>";
+
+			print format("
+				<tr class=\"header1\">
+					<th style=\"text-align: right;\" colspan=\"6\">
+						<input type=\"hidden\" id='actionfield' name=\"action\" value=\"multidel\" />
+						<a href=\"javascript:void();\" onclick=\"document.getElementById('actionfield').value = 'multidel'; document.forms[1].submit();\">".__("delete checked")."</a>
+						<a href=\"javascript:void();\" onclick=\"document.getElementById('actionfield').value = 'multimove'; document.forms[1].submit();\">".__("Move checked to")."</a>$movelist
+					</th>
+				</tr>");
+		}
+			print "</table>";
+	}
+}
+
+
+$cat = getCategory($_GET["cat"]);
+$links = actionLinkTag("Upload file", "uploader", "", "action=uploadform&cat=".$_GET["cat"]);
+
+if($_GET["cat"] == -2)
+	$links = "";
+if($isBot)
+	$links = "";
+if(!$loguserid)
+	$links = "";
+
+MakeCrumbs(array("Main"=>"./", 
+				"Uploader"=>actionLink("uploader"), 
+				$cat["name"] => actionLink("uploaderlist", "", "cat=".$cat["id"]), 
+				), $links);
+
+print "<form method=\"post\" action=\"".actionLink("uploader", "", "cat=${_GET["cat"]}")."\">";
+listCategory($_GET["cat"]);
+print "</form>";
+
+
+function getCategory($cat)
+{
+
+	if (!is_numeric($cat))
+		Kill('Invalid category');
+
+	if($cat >= 0)
+	{
+		$qCategory = "select * from uploader_categories where id=".$cat;
+		$rCategory = Query($qCategory);
+		if(NumRows($rCategory) == 0) Kill("Invalid category");
+		$rcat = Fetch($rCategory);
+	}
+	else if($cat == -1)
+		$rcat = array("id" => -1, "name" => "Private files");
+	else if($cat == -2)
+		$rcat = array("id" => -2, "name" => "All private files");
+	else
+		Kill('Invalid category');
+
+	return $rcat;
+}
+
+?>
