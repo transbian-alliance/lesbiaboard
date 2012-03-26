433cd6cb4e1c8c177606318a844812a741a9b298
Nina <nhp@laptop.localdomain>
2012-03-26 09:31:15 +0200

    Made the 404 page more detailed and added a link to return to the index page.

--- abxd/pages/404.php
+++ abxd/pages/404.php
@@ -2,12 +2,13 @@
 //  AcmlmBoard XD - 404
 //  Access: all
 
-// Some servers use one response, some use other. For safety, use both.
+// Some servers use one response, some use another. For safety, use both.
 header('HTTP/1.1 404 Not Found');
 header('Status: 404 Not Found');
 
 $title = __('404');
 
-Kill(__('404. Not found.'));
+Kill(__('Error 404 - The page you are looking for was not found.').'<br />
+	<a href=".">'.__('Return to the board index').'</a>');
 
 ?>
