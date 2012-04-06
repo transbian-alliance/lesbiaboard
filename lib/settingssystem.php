b862f5b0f11578858909cf419dd80fa16cf3422d
Dirbaio <dirbaio@dirbaio.net>
2012-04-06 17:58:59 +0200

    asdf

--- lib/settingssystem.php
+++ lib/settingssystem.php
@@ -1,7 +1,13 @@
 <?php
 
 /*
-	Setting system info:
+	I really don't like the idea of settings.php. Self-modifying code is bad, and 
+	it's like that just for historical reasons.
+	
+	We should now do one settings system that stores everything in the MySQL DB, and works
+	for all possible use cases.
+	
+	This is how the new Settings System should work:
 	
 	We have to store 4 types of settings:
 	- Board global settings
@@ -9,12 +15,31 @@
 	- Board per-user settings
 	- Plugin per-user settings
 	
-	Both types of global settings are stored in the "settings" table.
+	Global settings (both types) are stored in the "settings" table.
 	"plugin" field is the plugin name, or "main" if it's a board setting.
 	"name" field is the name of the setting.
 	"value" field is the value of the setting.
 	
-	TODO: Per-user settings (?)
+	TODO: specify how are per-user settings stored and implement them. 
+	
+	======
+	
+	Types of settings:
+	
+	- integer
+	- text			Creates a text field
+	- textbig		Creates a text box with no controls
+	- textbbcode	Creates a text box with BBCode post help
+	- texthtml		Creates a text box with HTML post help (if it's ever implemented)
+
+	- theme			Creates a theme selection drop-down. Stores the theme name.
+	- user 			Creates a user selection drop-down. Stores the UID
+	- forum			Creates a forum selection drop-down. Stores the FID
+	
+	Additionally settings can have a default value.	
+	Also there should be some validation of the setting values.
+	Specially for per-user settings, which can be modified at will by users.
+
 */
 
 //Loads ALL the settings.
@@ -26,6 +51,16 @@ function loadSettings()
 	$pluginsettings = array();
 	$globalsettings = array();
 	$rSettings = Query("select * from settings");
-	while $setting
+	while($setting = Fetch($rSettings))
+	{
+		if($setting["plugin"] == "main")
+			$globalsettings[$setting["name"]] = $setting["value"];
+		else
+			$pluginsettings[$setting["plugin"]][$setting["name"]] = $setting["value"];
+	}
 }
+
+//TODO: Functions to change settings.
+//TODO: Setting Description Files
+//TODO: Having the board actually use these settings.
 ?>
