41a6f6b051ee64b4ae62798f610a7292bbe66c69
Dirbaio <dirbaio@dirbaio.net>
2012-04-15 14:44:57 +0200

    Whoops forgot a file

---
+++ header.php
@@ -0,0 +1,14 @@
+	<meta http-equiv="Content-Type" content="text/html; CHARSET=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=9" />
+	<meta name="description" content="<?php print $metaDescription; ?>" />
+	<meta name="keywords" content="<?php print $metaKeywords; ?>" />
+	<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="<?php print resourceLink("css/common.css");?>" />
+	<link rel="stylesheet" type="text/css" id="theme_css" href="<?php print $layout_themefile; ?>" /> 
+
+	<script type="text/javascript" src="<?php print resourceLink("lib/tricks.js");?>"></script>
+	<script type="text/javascript" src="<?php print resourceLink("lib/jquery.js");?>"></script>
+	<script type="text/javascript" src="lib/jquery.tablednd_0_5.js"></script>
+	<?php
+		$bucket = "pageHeader"; include("./lib/pluginloader.php");
+	?>
