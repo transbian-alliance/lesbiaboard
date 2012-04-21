c63c021bc284737e9706c65b9c8042e4b80d8892
Mega-Mario <thetotalworm@gmail.com>
2012-04-21 15:04:14 +0200

    editpost: reimplement 'editing last post marks thread unread' so that it doesn't alter the post's date

--- pages/editpost.php
+++ pages/editpost.php
@@ -142,10 +142,7 @@ if($_POST['action'] == __("Edit"))
 		//Update thread lastpostdate if we edited the last post
 		if($wasLastPost)
 		{
-			$qThreads = "update threads set lastpostdate=".time()." where id=".$tid." limit 1";
-			$qPosts = "update posts set date=".time()." where id=".$pid." limit 1";
-			$rThreads = Query($qThreads);
-			$rPosts = Query($qPosts);
+			Query("DELETE FROM threadsread WHERE thread={$thread['id']}");
 		}
 
 		if($forum['minpower'] < 1)
