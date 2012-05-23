26189263ce846d035aa9ad35e7dc720418e2dd7a
GlitchMr <glitch.mr@gmail.com>
2012-05-23 19:45:13 +0200

    New tool - findfunction.php, it finds functions everywhere.

---
+++ tools/findfunction.php
@@ -0,0 +1,26 @@
+<?php
+function search_for_function($tokens, $filename)
+{
+	global $function;
+	foreach ($tokens as $id => $token)
+	{
+		if (is_array($token) && $token[0] === T_FUNCTION && is_array($tokens[$id + 1]) && strtolower($tokens[$id + 1][1]) === $function)
+		{
+			die("$function() is defined in $filename, line {$tokens[$id + 1][2]}.\n");
+		}
+	}
+}
+
+require '../lib/recursivetokenizer.php';
+if (!isset($argv[1]))
+{
+	die("Usage: $argv[0] [FUNCTION]\n");
+}
+$function = strtolower($argv[1]);
+$php_functions = get_defined_functions();
+if (in_array($function, $php_functions['internal']))
+{
+	die("$function() is PHP core function.\n");
+}
+recurse('search_for_function');
+echo "$argv[1]() wasn't found.\n";
