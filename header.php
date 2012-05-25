e5db56756a1332a9968076760263d062949e1bcb
GlitchMr <glitchmr@myopera.com>
2012-05-25 17:15:43 +0200

    Fix lack of resourceLink() in header.php

--- header.php
+++ header.php
@@ -2,13 +2,13 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=9" />
 	<meta name="description" content="<?php print $metaDescription; ?>" />
 	<meta name="keywords" content="<?php print $metaKeywords; ?>" />
-	<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+	<link rel="shortcut icon" type="image/x-icon" href="<?php print resourceLink("img/favicon.ico");?>" />
 	<link rel="stylesheet" type="text/css" href="<?php print resourceLink("css/common.css");?>" />
 	<link rel="stylesheet" type="text/css" id="theme_css" href="<?php print $layout_themefile; ?>" /> 
 
 	<script type="text/javascript" src="<?php print resourceLink("lib/jquery.js");?>"></script>
 	<script type="text/javascript" src="<?php print resourceLink("lib/tricks.js");?>"></script>
-	<script type="text/javascript" src="lib/jquery.tablednd_0_5.js"></script>
+	<script type="text/javascript" src="<?php print resourceLink("lib/jquery.tablednd_0_5.js");?>"></script>
 	<?php
 		$bucket = "pageHeader"; include("./lib/pluginloader.php");
 	?>
