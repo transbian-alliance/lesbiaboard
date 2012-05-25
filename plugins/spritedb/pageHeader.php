dcedd11ee7f94eb5d90bb40ea7dc556c2db2e05a
Dirbaio <dirbaio@dirbaio.net>
2012-05-25 22:34:32 +0200

    Deleted sprite DB plugin.

--- plugins/spritedb/pageHeader.php
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript" src="plugins/spritedb/tricks.js"></script>
-<link rel="stylesheet" type="text/css" href="plugins/spritedb/style.css"/>
-
-
-
--- plugins/spritedb/topMenu.php
+++
@@ -1,3 +0,0 @@
-<?
-	print actionLinkTagItem(__("Sprite Database"), "spritedb");
-
