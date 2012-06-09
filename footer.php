b272931f2be5ce16a853e9feb4f6000cf9088dd9
Dirbaio <dirbaio@dirbaio.net>
2012-06-09 19:55:44 +0200

    You deserve it! er

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <?php print $footerExtensionsA?>
 
 Powered by <a href="https://github.com/Dirbaio/ABXD">AcmlmBoard XD</a><br />
-By Dirbaio, Kawa, Mega-Mario, Nikolaj, et al<br />
+By Dirbaio, Kawa, Mega-Mario, Nikolaj, GlitchMr et al<br />
 AcmlmBoard &copy; Jean-Fran&ccedil;ois Lapointe<br />
 Page rendered in <?php print sprintf("%1.3f",usectime()-$timeStart)?> seconds with <?php print Plural($queries, __("MySQL query"))?> <br />
 <?php print __("<!-- English translation by The ABXD Team -->")?>
