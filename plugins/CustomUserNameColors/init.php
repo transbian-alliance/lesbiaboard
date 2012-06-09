9fcb8fcd9963f4c1c69879162a1919e62d33d756
GlitchMr <glitchmr@myopera.com>
2012-06-09 17:45:22 +0200

    Fix CustomUserNameColors
    Ok, let's get it straight, somebody broken it (I don't know why). I've
    copied script from Undefined Zone (running on ABXD 2.2.3) and it worked.

--- plugins/CustomUserNameColors/init.php
+++ plugins/CustomUserNameColors/init.php
@@ -1,4 +1,10 @@
 <?php
-$uncolors = file_get_contents("./plugins/".$plugins[$plugin]['dir']."/colors");
-$uncolors = unserialize($uncolors);
+if (file_exists("./plugins/".$plugins[$plugin]['dir']."/colors")) {
+	$uncolors = file_get_contents("./plugins/".$plugins[$plugin]['dir']."/colors");
+	$uncolors = unserialize($uncolors);
+}
+else {
+	file_put_contents("./plugins/".$plugins[$plugin]['dir']."/colors", serialize(array()));
+	$uncolors = array();
+}
 ?>
