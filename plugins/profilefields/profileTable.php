12b069e6b8de03e9013652195646d2d12c92b115
GlitchMr <glitchmr@myopera.com>
2012-06-09 19:29:54 +0200

    i18n

--- plugins/profilefields/profileTable.php
+++ plugins/profilefields/profileTable.php
@@ -3,7 +3,7 @@ for($i = 0; $i < $selfsettings['numberOfFields']; $i++)
 {
 	if(getSetting("profileExt".$i."t", true) != "" && getSetting("profileExt".$i."v", true) != "")
 	{
-		$profileParts['Other stuff'][strip_tags(getSetting("profileExt".$i."t", true))] = CleanUpPost(getSetting("profileExt".$i."v", true));
+		$profileParts[__('Other stuff')][strip_tags(getSetting("profileExt".$i."t", true))] = CleanUpPost(getSetting("profileExt".$i."v", true));
 	}
 }
 
