132b1e19703f2c6b0b19880a61ce07db369e8a1f
Mega-Mario <thetotalworm@gmail.com>
2012-07-01 19:59:14 +0200

    Add new plugin: show the current user how many users have blocked their layout. Hopefully will help noobs realize when their layouts suck.

---
+++ plugins/layoutblockstats/sidebar.php
@@ -0,0 +1,10 @@
+<?php
+
+if ($post['uid'] != $loguserid) return;
+
+static $myblockcount = -1;
+if ($myblockcount == -1) $myblockcount = FetchResult("SELECT COUNT(*) FROM blockedlayouts WHERE blockee={$loguserid}");
+
+$sideBarStuff .= "<br>".$myblockcount.($myblockcount==1 ? ' user has':' users have')." blocked your layout<br>";
+
+?>
