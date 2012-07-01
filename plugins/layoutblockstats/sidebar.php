ebe13c88517f6d884070e7d8fc8ee03135d522f5
Dirbaio <dirbaio@dirbaio.net>
2012-07-01 22:13:52 +0400

    Small fix...

--- plugins/layoutblockstats/sidebar.php
+++ plugins/layoutblockstats/sidebar.php
@@ -1,10 +1,11 @@
 <?php
 
-if ($post['uid'] != $loguserid) return;
-
-static $myblockcount = -1;
-if ($myblockcount == -1) $myblockcount = FetchResult("SELECT COUNT(*) FROM blockedlayouts WHERE blockee={$loguserid}");
-
-$sideBarStuff .= "<br>".$myblockcount.($myblockcount==1 ? ' user has':' users have')." blocked your layout<br>";
+if ($post['uid'] == $loguserid)
+{
+	static $myblockcount = -1;
+	if ($myblockcount == -1) $myblockcount = FetchResult("SELECT COUNT(*) FROM blockedlayouts WHERE blockee={$loguserid}");
+	
+	$sideBarStuff .= "<br>".$myblockcount.($myblockcount==1 ? ' user has':' users have')." blocked your layout<br>";
+}
 
 ?>
