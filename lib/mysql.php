bbcc02f85a11e533b26e99bf00e560b7ec9fa8d5
GlitchMr <glitchmr@myopera.com>
2012-07-14 18:11:50 +0200

    Remove justEscape() function so PHP error will appear instead.

--- lib/mysql.php
+++ lib/mysql.php
@@ -15,12 +15,6 @@ function SqlEscape($text)
 	return $dblink->real_escape_string($text);
 }
 
-function justEscape($text) {
-	$backtrace = debug_backtrace();
-	Kill("FIXME: {$backtrace[0]['file']} ({$backtrace[0]['line']})");
-	return $text;
-}
-
 function Query_ExpandFieldLists($match)
 {
 	$ret = array();
