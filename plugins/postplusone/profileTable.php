1d92c3e266f6beec6895dac839db40a8fc75266a
Dirbaio <dirbaio@dirbaio.net>
2012-08-06 23:27:06 +0200

    dafajewoibmiwjegacg

--- plugins/postplusone/profileTable.php
+++ plugins/postplusone/profileTable.php
@@ -1,4 +1,4 @@
 <?php
 
-$profileParts[__("General information")][__("Total +1's received")] = $user["postplusones"];
-$profileParts[__("General information")][__("Total +1's given")] = $user["postplusonesgiven"];
+$profileParts[__("General information")][__("Total +1s received")] = $user["postplusones"];
+$profileParts[__("General information")][__("Total +1s given")] = $user["postplusonesgiven"];
