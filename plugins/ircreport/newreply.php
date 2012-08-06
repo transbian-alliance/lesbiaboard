b941b355394f0604d84f58f4981396e724153d08
Nina <nina@localhost.localdomain>
2012-08-06 04:04:00 +0200

    More oops.

--- plugins/ircreport/newreply.php
+++ plugins/ircreport/newreply.php
@@ -9,7 +9,7 @@ if($loguser["displayname"])
 	
 if ($forum['minpower'] <= 0)
 	ircReport("\003".$c2."New reply by\003$c1 "
-		.ircUserColor($thename, $loguser['sex'], $loguser['power'])
+		.ircUserColor($thename, $loguser['sex'], $loguser['powerlevel'])
 		."\003$c2: \003$c1"
 		.$thread["title"]
 		."\003$c2 (".$forum["title"].")"
--- plugins/ircreport/newthread.php
+++ plugins/ircreport/newthread.php
@@ -10,7 +10,7 @@ if($loguser["displayname"])
 
 if ($forum['minpower'] <= 0)
 	ircReport("\003".$c2."New thread by\003$c1 "
-		.ircUserColor($thename, $loguser['sex'], $loguser['power'])
+		.ircUserColor($thename, $loguser['sex'], $loguser['powerlevel'])
 		."\003$c2: \003$c1"
 		.$thread["title"]
 		."\003$c2 (".$forum["title"].")"
