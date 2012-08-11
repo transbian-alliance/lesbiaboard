7b41171aef244c750dd36888b3ad328e1ee461b5
Dirbaio <dirbaio@dirbaio.net>
2012-08-11 03:23:51 +0200

    Fixed menu not scrolling since start of the page.

--- layouts/nsmbhd-scroll.js
+++ layouts/nsmbhd-scroll.js
@@ -1,36 +1,40 @@
+"use strict"
 
-$(function() {
+function doScroll() {
 	var navigation = $("#navigation");
 	var userpanel = $("#userpanel");
 	var userpanelPH = $("#userpanel-placeholder");
 
 	var view = $(window);
+	var viewTop = view.scrollTop();
+
+	var sidebarTop = $("#sidebar").offset().top;
+	var margin = 35;
 	
-	view.bind("scroll resize", function() {
-		var viewTop = view.scrollTop();
+	if ((viewTop > sidebarTop - margin) && !navigation.is(".navigation-fixed"))
+		navigation.addClass("navigation-fixed");
+	else if ((viewTop <= sidebarTop - margin) && navigation.is(".navigation-fixed"))
+		navigation.removeClass("navigation-fixed");
 
-		var sidebarTop = $("#sidebar").offset().top;
-		var margin = 35;
-		
-		if ((viewTop > sidebarTop - margin) && !navigation.is(".navigation-fixed"))
-			navigation.addClass("navigation-fixed");
-		else if ((viewTop <= sidebarTop - margin) && navigation.is(".navigation-fixed"))
-			navigation.removeClass("navigation-fixed");
+	var userpanelTop = userpanelPH.offset().top;
 
-		var userpanelTop = userpanelPH.offset().top;
+	if ((viewTop > userpanelTop) && !userpanel.is(".userpanel-fixed"))
+	{
+		userpanelPH.height(userpanelPH.height());
+		userpanelPH.width(userpanelPH.width());
+		userpanel.addClass("userpanel-fixed");
+	}
+	else if ((viewTop <= userpanelTop) && userpanel.is(".userpanel-fixed"))
+	{
+		userpanelPH.css("height", "auto");
+		userpanelPH.css("width", "auto");
+		userpanel.removeClass("userpanel-fixed");
+	}
+}
 
-		if ((viewTop > userpanelTop) && !userpanel.is(".userpanel-fixed"))
-		{
-			userpanelPH.height(userpanelPH.height());
-			userpanelPH.width(userpanelPH.width());
-			userpanel.addClass("userpanel-fixed");
-		}
-		else if ((viewTop <= userpanelTop) && userpanel.is(".userpanel-fixed"))
-		{
-			userpanelPH.css("height", "auto");
-			userpanelPH.css("width", "auto");
-			userpanel.removeClass("userpanel-fixed");
-		}
-	});
+$(function() {
+	var view = $(window);
+	view.bind("scroll resize", doScroll);
+	doScroll();
 });
  
