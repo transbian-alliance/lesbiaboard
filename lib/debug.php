c73802cccbb43b31885e5025b0140d3e41c24b3f
GlitchMr <glitchmr@myopera.com>
2012-08-14 18:22:05 +0200

    Use tabs instead of spaces

--- lib/debug.php
+++ lib/debug.php
@@ -3,28 +3,28 @@
 
 function backTrace($backtrace)
 {
-    foreach ($backtrace as $bt) {
-        $args = '';
-        foreach ($bt['args'] as $a) {
-            if ($args) {
-                $args .= ', ';
-            }
-            if (in_array($bt['function'], array('RawQuery', 'Query', 'FetchResult')) && !$args)
-                $args .= '"..."';
-            else
-                $args .= var_export($a, true);
-        }
-        $output .= "<td>{$bt['file']}<td>{$bt['line']}<td><code>";
-        $output .= htmlspecialchars("{$bt['class']}{$bt['type']}{$bt['function']}($args)");
-        $output .= "</code><tr class=cell0>";
-    }
-    return $output;
+	foreach ($backtrace as $bt) {
+		$args = '';
+		foreach ($bt['args'] as $a) {
+			if ($args) {
+				$args .= ', ';
+			}
+			if (in_array($bt['function'], array('RawQuery', 'Query', 'FetchResult')) && !$args)
+				$args .= '"..."';
+			else
+				$args .= var_export($a, true);
+		}
+		$output .= "<td>{$bt['file']}<td>{$bt['line']}<td><code>";
+		$output .= htmlspecialchars("{$bt['class']}{$bt['type']}{$bt['function']}($args)");
+		$output .= "</code><tr class=cell0>";
+	}
+	return $output;
 }
 
 
 function var_format($v) // pretty-print var_export
 {
-    return (str_replace(array("\n"," ","array"),
+	return (str_replace(array("\n"," ","array"),
 array("<br>","&nbsp;","&nbsp;<i>array</i>"),
 var_export($v,true))."<br>");
 }
