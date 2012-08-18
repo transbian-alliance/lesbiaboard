c67e0b1583d850c5347e423515883f494c5b96f9
GlitchMr <glitchmr@myopera.com>
2012-08-18 11:57:22 +0200

    Add site performance plugin

--- footer.php
+++ footer.php
@@ -5,7 +5,6 @@
 Powered by <a href="https://github.com/Dirbaio/ABXD">AcmlmBoard XD</a><br />
 By Dirbaio, Kawa, Mega-Mario, Nikolaj, GlitchMr et al<br />
 AcmlmBoard &copy; Jean-Fran&ccedil;ois Lapointe<br />
-Page rendered in <?php print sprintf("%1.3f",usectime()-$timeStart)?> seconds with <?php print Plural($queries, __("MySQL query"))?> <br />
 <?php print __("<!-- English translation by The ABXD Team -->")?>
 
 <?php print $footerButtons?>
