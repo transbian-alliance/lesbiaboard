a97ccd74cfd001003c9491a1f46459ec0a33acad
Nina <nina@localhost.localdomain>
2012-09-21 21:22:01 +0200

    Attempt to make it a little nicer on phones.

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=9" />
 	<meta name="description" content="<?php print Settings::get('metaDescription'); ?>" />
 	<meta name="keywords" content="<?php print Settings::get('metaTags'); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<link rel="shortcut icon" type="image/x-icon" href="<?php print $layout_favicon;?>" />
 	<link rel="stylesheet" type="text/css" href="<?php print resourceLink("css/common.css");?>" />
 	<link rel="stylesheet" type="text/css" id="theme_css" href="<?php print resourceLink($layout_themefile); ?>" /> 
