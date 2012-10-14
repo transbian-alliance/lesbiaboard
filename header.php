fd9253e530ee15e0a05f19ebbb9b1ebf72d8c767
Nina <ninahp64@gmail.com>
2012-10-14 11:44:45 +0200

    Set mode for IE to IE10

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 	<meta http-equiv="Content-Type" content="text/html; CHARSET=utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=9" />
+	<meta http-equiv="X-UA-Compatible" content="IE=10" />
 	<meta name="description" content="<?php print Settings::get('metaDescription'); ?>" />
 	<meta name="keywords" content="<?php print Settings::get('metaTags'); ?>" />
 	<link rel="shortcut icon" type="image/x-icon" href="<?php print $layout_favicon;?>" />
