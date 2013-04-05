74b27889e7f77b172f8a6c62c6f02d54af8e7484
Nina <ninahp64@gmail.com>
2013-04-05 14:24:25 +0200

    Added transition for the navigation bar in the mobile layout and disallowed scrolling while the navigation bar is shown.

--- layouts/mobile.js
+++ layouts/mobile.js
@@ -1,13 +1,32 @@
 var headerShown = false;
 
+
+
 function mobile_openHeader() {
 	$('.mobile_openHeader').toggleClass("selected");
-	if(headerShown) {
-		$("#mobile_header").removeClass("shown");
+
+	/* This is used for preventing scrolling on the body element */
+	var body = document.getElementsByTagName("body")[0];
+	var pContents = document.getElementById("page_contents");
+	var header = document.getElementById("mobile_header");
+
+	if(!headerShown) {
+		$(header).addClass("shown");
+		pContents.addEventListener("touchstart", closeOnTouch, false);
+		body.addEventListener("touchmove", bListener, false);
 	} else {
-		$("#mobile_header").addClass("shown");
+		$(header).removeClass("shown");
+		pContents.removeEventListener("touchstart", closeOnTouch, false);
+		body.removeEventListener("touchmove", bListener, false);
 	}
 
 	headerShown = !headerShown;
 }
 
+var closeOnTouch = function(event) {
+	mobile_openHeader();
+};
+
+var bListener = function(event) {
+	event.preventDefault();
+};
