67829e389468b2f7261a11631f5fd5d58fb8ab28
Dirbaio <dirbaio@dirbaio.net>
2013-04-05 17:31:49 +0200

    It doesn't work well in some browsers :( Sorry Nina! Revert "Added transition for the navigation bar in the mobile layout and"
    This reverts commit 74b27889e7f77b172f8a6c62c6f02d54af8e7484.

--- layouts/mobile.js
+++ layouts/mobile.js
@@ -1,32 +1,13 @@
 var headerShown = false;
 
-
-
 function mobile_openHeader() {
 	$('.mobile_openHeader').toggleClass("selected");
-
-	/* This is used for preventing scrolling on the body element */
-	var body = document.getElementsByTagName("body")[0];
-	var pContents = document.getElementById("page_contents");
-	var header = document.getElementById("mobile_header");
-
-	if(!headerShown) {
-		$(header).addClass("shown");
-		pContents.addEventListener("touchstart", closeOnTouch, false);
-		body.addEventListener("touchmove", bListener, false);
+	if(headerShown) {
+		$("#mobile_header").removeClass("shown");
 	} else {
-		$(header).removeClass("shown");
-		pContents.removeEventListener("touchstart", closeOnTouch, false);
-		body.removeEventListener("touchmove", bListener, false);
+		$("#mobile_header").addClass("shown");
 	}
 
 	headerShown = !headerShown;
 }
 
-var closeOnTouch = function(event) {
-	mobile_openHeader();
-};
-
-var bListener = function(event) {
-	event.preventDefault();
-};
