1d2a3c70fc86368454c862270ec2fe3fbefa0304
Konrad Borowski <glitchmr@myopera.com>
2013-04-21 13:35:59 +0200

    Hide logsession

--- plugins/badbehavior/page_abusers.php
+++ plugins/badbehavior/page_abusers.php
@@ -48,7 +48,7 @@ while ($abuser = fetch($abusers))
 				".formatIP($abuser['ip'])."
 			</td>
 			<td>
-				<pre>". htmlspecialchars($abuser['http_headers']) . "</pre>
+				<pre>". htmlspecialchars(preg_replace('/logsession=\w+/', 'logsession=?????', $abuser['http_headers'])) . "</pre>
 			</td>
 			<td>
 				{$abuser['key']}
