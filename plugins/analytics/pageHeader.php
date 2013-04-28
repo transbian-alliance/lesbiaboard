a9e3bf89f365790519e0811d52181727610894dd
Dirbaio <dirbaio@dirbaio.net>
2013-04-29 01:27:30 +0200

    Analytics now tracks logged in/out state.

--- plugins/analytics/pageHeader.php
+++ plugins/analytics/pageHeader.php
@@ -4,6 +4,8 @@
   _gaq.push(['_setAccount', '<?php print trim(Settings::pluginGet("trackingid"));?>']);
   _gaq.push(['_trackPageview']);
 
+ _gaq.push(['_setCustomVar', 1, 'Logged in', '<?php print $loguserid?"Yes":"No";?>', 2]);
+   
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
