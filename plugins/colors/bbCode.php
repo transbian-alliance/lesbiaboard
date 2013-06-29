6c3a2949f15f9359d666e930081d3fff1aa6f95f
Konrad Borowski <glitchmr@myopera.com>
2013-06-29 15:21:40 +0200

    Color module

--- plugins/colors/bbCode.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	$s = preg_replace("'\[color=[\'\"]([A-Za-z#0-9]*?)[\'\"]\](.*?)\[/color\]'si","<span style=\"color: \\1\">\\2</span>", $s);
-	$s = preg_replace("'\[color=([A-Za-z#0-9]*?)\](.*?)\[/color\]'si","<span style=\"color: \\1\">\\2</span>", $s);
-
-?>
