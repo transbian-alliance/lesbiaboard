10c509c10a0dd5efd96700f05619fee653eb5a30
Konrad Borowski <glitchmr@myopera.com>
2013-07-10 20:43:50 +0200

    Revert "Allow branch changes"
    This reverts commit 73a31cdf1f2bc847d76100b15d4479c72544614b.

--- plugins/gitpull/page_gitpull.php
+++ plugins/gitpull/page_gitpull.php
@@ -2,12 +2,6 @@
 if($loguser['powerlevel'] < 2)
 	Kill(__("You're not admin. There is nothing for you here."));
 
-if(isset($_GET['branch']) && preg_match('/^[\w-]+$/', $_GET['branch']))
-{
-	exec("git checkout $_GET[branch]");
-	return;
-}
-
 $crumbs = new PipeMenu();
 $crumbs->add(new PipeMenuLinkEntry(__("Admin"), "admin"));
 $crumbs->add(new PipeMenuLinkEntry(__("Update board"), "gitpull"));
