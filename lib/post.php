d41572c50078271642f2de7779462c5e42db8860
Konrad Borowski <glitchmr@myopera.com>
2013-07-19 11:45:37 +0200

    here too

--- lib/post.php
+++ lib/post.php
@@ -137,7 +137,7 @@ function makePostText($post)
 		{
 			$postLayout = $poster['postheader'].$magicString.$poster['signature'];
 			$postLayout = ApplyTags($postLayout, $tags);
-			$postLayout = CleanUpPost($postLayout, $poster['name'], $noSmilies, false);
+			$postLayout = CleanUpPost($postLayout, $poster['name'], $noSmilies);
 			$layoutCache[$poster["id"]] = $postLayout;
 		}
 		else
