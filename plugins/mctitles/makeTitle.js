67d3dfdb9d5cd527df525447663f41ab632ec016
Nina <ninahp64@gmail.com>
2013-08-02 01:40:07 +0200

    Improved Minecraft titles plugin. Someone please fix the titles setting. I have no idea what's wrong with it.

--- plugins/mctitles/makeTitle.js
+++ plugins/mctitles/makeTitle.js
@@ -6,34 +6,11 @@
  * ~Nina
  */
 
-window.addEventListener("load", function() {
-	var titles = [
-		"Open source!",
-		"...and freely distributable!",
-		"Available on GitHub!",
-		"Supports AJAX!",
-		"AcmlmBoard XD!",
-		"May contain JavaScript!",
-		"Fork it!",
-		"Internationalized!",
-		"What?",
-		"Using PHP!",
-		":() { :|: & };:",
-		"Not made by Acmlm!",
-		"Engage!",
-		"Call to undefined function!",
-		"Colorful!",
-		"Doesn't use Tidy!",
-		"Probably has bugs!"
-	];
-
-	var title = titles[Math.round(Math.random() * (titles.length - 1))];
-
+var makeMcTitle = function(title) {
 	var boardHeader = document.getElementById("theme_banner");
 	var mcTitle = document.createElement("span");
 	mcTitle.className = "mcTitle";
 	mcTitle.appendChild(document.createTextNode(title));
 
 	boardHeader.parentNode.parentNode.insertBefore(mcTitle, boardHeader.parentNode.nextSibling);
-
-}, false);
+};
--- plugins/mctitles/pageHeader.php
+++ plugins/mctitles/pageHeader.php
@@ -1,2 +1,14 @@
+<?php
+$mcTitles = Settings::pluginGet("titles");
+$mcTitles = explode("\n", $mcTitles);
+	
+$mcTitle = $mcTitles[array_rand($mcTitles)];
+?>
+
 	<script type="text/javascript" src="<?php print resourceLink("plugins/mctitles/makeTitle.js");?>"></script>
+	<script type="text/javascript">
+		window.addEventListener("load", function() {
+			makeMcTitle("<?php print $mcTitle; ?>");
+		}, false);
+	</script>
 	<link rel="stylesheet" type="text/css" href="<?php print resourceLink("plugins/mctitles/mctitles.css");?>" />
