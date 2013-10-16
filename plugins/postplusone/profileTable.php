b79b01f4d7318ee130b18ccabd45413fe7a9b376
Dirbaio <dirbaio@dirbaio.net>
2013-10-17 01:34:59 +0200

    Show top plusoners in profile page.

--- plugins/postplusone/profileTable.php
+++ plugins/postplusone/profileTable.php
@@ -6,3 +6,21 @@ if($user["postplusones"])
 	
 $profileParts[__("General information")][__("Total +1s received")] = $val;
 $profileParts[__("General information")][__("Total +1s given")] = $user["postplusonesgiven"];
+
+$res = query("select count(*) as ct, u.(_userfields)
+from postplusones l
+left join posts p on l.post=p.id
+left join users u on u.id = l.user
+where p.user={0}
+group by l.user
+order by count(*) desc
+limit 5", $user["id"]);
+
+$plusoners = array();
+
+while($row = fetch($res))
+	$plusoners[] = userLink(getDataPrefix($row, "u_"))." (".$row["ct"].")";
+
+$profileParts[__("General information")][__("Top +1ers")] = implode(", ", $plusoners);
+
+
