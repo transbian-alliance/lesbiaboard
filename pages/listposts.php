e1bf2e2f613daf8c7fa47c0e7b8964497647bb6d
Dirbaio Minikiwi <dirbaio@dirbaio.net>
2013-12-06 02:34:40 +0100

    STFU cusell

--- pages/listposts.php
+++ pages/listposts.php
@@ -36,24 +36,26 @@ else
 if(!$ppp) $ppp = 25;
 
 
-$rPosts = Query("	SELECT
-				p.*,
-				pt.text, pt.revision, pt.user AS revuser, pt.date AS revdate,
-				u.(_userfields), u.(rankset,title,picture,posts,postheader,signature,signsep,lastposttime,lastactivity,regdate,globalblock),
-				ru.(_userfields),
-				du.(_userfields),
-				t.id thread, t.title threadname,
-				f.id fid
-			FROM
-				{posts} p
-				LEFT JOIN {posts_text} pt ON pt.pid = p.id AND pt.revision = p.currentrevision
-				LEFT JOIN {users} u ON u.id = p.user
-				LEFT JOIN {users} ru ON ru.id=pt.user
-				LEFT JOIN {users} du ON du.id=p.deletedby
-				LEFT JOIN {threads} t ON t.id=p.thread
-				LEFT JOIN {forums} f ON f.id=t.forum
-			WHERE u.id={1} AND ".forumAccessControlSql()."
-			ORDER BY date ASC LIMIT {2u}, {3u}", $loguserid, $id, $from, $ppp);
+$rPosts = Query("
+	SELECT
+		p.*,
+		pt.text, pt.revision, pt.user AS revuser, pt.date AS revdate,
+		u.(_userfields), u.(rankset,title,picture,posts,postheader,signature,signsep,lastposttime,lastactivity,regdate,globalblock),
+		ru.(_userfields),
+		du.(_userfields),
+		t.id thread, t.title threadname,
+		f.id fid
+	FROM
+		{posts} p
+		LEFT JOIN {posts_text} pt ON pt.pid = p.id AND pt.revision = p.currentrevision
+		LEFT JOIN {users} u ON u.id = p.user
+		LEFT JOIN {users} ru ON ru.id=pt.user
+		LEFT JOIN {users} du ON du.id=p.deletedby
+		LEFT JOIN {threads} t ON t.id=p.thread
+		LEFT JOIN {forums} f ON f.id=t.forum
+	WHERE u.id={1} AND ".forumAccessControlSql()."
+	ORDER BY date ASC LIMIT {2u}, {3u}", 
+	$loguserid, $id, $from, $ppp);
 
 $numonpage = NumRows($rPosts);
 
