d5d779dbb4542692f701aada25826fd9537f1184
maple syrup <maple@maple.pet>
2018-07-08 04:33:44 -0300

    get rid of mario rankset and add standard ranks

---
+++ ranksets/standard/rankset.php
@@ -0,0 +1,12 @@
+<?php
+
+$ranksetNames["standard"] = "Standard";
+
+$ranksetData["standard"] = array(
+	array("num" => 0, "image" => '', "text" => 'Non-poster'),
+	array("num" => 1, "image" => '', "text" => 'Newcomer'),
+	array("num" => 100, "image" => '', "text" => 'Member'),
+	array("num" => 500, "image" => '', "text" => 'Regular'),
+	array("num" => 1000, "image" => '', "text" => 'Active'),
+	array("num" => 2000, "image" => '', "text" => 'Way past cool')
+);
