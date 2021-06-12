75f1b7c2581f2a278b4f265a171c8e2b8f0fc5e4
Pululut <85770889+Pululut@users.noreply.github.com>
2021-06-12 01:45:54 +0100

    Footer version now determined from version.php

--- footer.php
+++ footer.php
@@ -1,14 +1,13 @@
 <?php
+//TODO: proper credits page -- Pululut
 if (!$mobileLayout) echo '<a href="#" onclick="enableMobileLayout(1); return false;" rel="nofollow">Mobile view</a>';
 else echo '<a href="#" onclick="enableMobileLayout(-1); return false;" rel="nofollow">Disable mobile view</a>';
 ?>
 <br>
 <br>
-<!-- TODO: pull version from somewhere else -->
 <?php $bucket = "footer"; include("./lib/pluginloader.php");?>
-Powered by Lesbiaboard 1.3 by <a href="http://transbian.love/">Transbian Alliance</a><br />
-Acmlmboard XD &copy; Dirbaio, xfix, Kawa, StapleButter, Nadia, et al<br />
-AcmlmBoard &copy; Jean-Fran&ccedil;ois Lapointe<br />
+Powered by Lesbiaboard <?php doBoardVersionFooter(); ?><br />
+&copy; 2018-<?php print date("Y"); ?> <a href="https://transbian.love/">Transbian Alliance</a><br />
 <?php print __("<!-- English translation by The ABXD Team -->")?>
 
 <?php print (isset($footerButtons) ? $footerButtons : "")?>
